7c53e29759b61f1b5fa141871b05ae62027595c6
St Elmo <stelmozors@gmail.com>
2012-02-25 18:17:08 +0200

    weighted cluster ranking algorithm

---
+++ clusterRank.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 25 17:09:34 2012
+
+@author: St Elmo Wilken
+"""
+
+class cRanking:
+    
+    def __init__(self,inMat1,inMat2):
+        pass
+    
+    def construcRankArray(self):
+        pass
+    
+    def showConnectRank(self):
+        pass
+
