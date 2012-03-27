8e555fb19498fa9f45b901224e036c69c9617554
St Elmo <stelmozors@gmail.com>
2012-03-27 20:13:38 +0200

    localchangematrix and connection matrix coded

--- localGainCalculator.py
+++ localGainCalculator.py
@@ -6,17 +6,37 @@ Created on Mon Mar 26 23:12:10 2012
 """
 
 #basically im trying to automate the local gain assigning mission... 
-#this wont seem useful just quite yet.. but just hold on
+#this wont seem useful just quite yet.. but hold on
 
 class localgains:
     
-    def __init__(self,nameofconn):
+    def __init__(self, nameofconn, nameofgains):
         self.createConn(nameofconn)
+        self.createLocalChangeMatrix(nameofgains)
+        
     
-    def createConn(self,nameofconn): # assigns variable names and connection scheme
+    def assignLocalGains(self):
+        #this method should calculate average gains based on the connectivity matrix and the local change matrix
+        pass
+    
+    def createLocalChangeMatrix(self, nameofgains):
+        #this method should return an array of local changes   
+        #the variable name are already in order due to foresight (hopefully)
+        import csv
+        fromfile = csv.reader(open(nameofgains),delimiter=' ')
+        self.localchangematrix = []
+        for line in fromfile:
+            linefixed = line[1:] #to get rid of a white space preceeding every line
+            for element in linefixed:
+                self.localchangematrix.append(float(element))
+        from numpy import array
+        self.localchangematrix = array(self.localchangematrix).reshape(len(self.variables),13) #12 input changes + 1 base case
+        #reasonably sure this works well
+    
+    def createConn(self, nameofconn): # assigns variable names and connection scheme
         import csv
         from numpy import array
-        fromfile = csv.reader(open(nameofconn));
+        fromfile = csv.reader(open(nameofconn))
         #first row: empty space, var1, var2, var3, ... varN
         #second row: var1, gain1, gain2, gain3, ... gainN
         self.variables = fromfile.next()[1:] #gets rid of that first space. Now the variables are all stored
