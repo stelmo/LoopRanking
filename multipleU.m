d4f3179fc049e28c130c28e3b664d5d02404047d
St Elmo <stelmozors@gmail.com>
2012-03-27 20:15:34 +0200

    multiple column output and incorporated full 45 variable (ordered)

--- multipleU.m
+++ multipleU.m
@@ -1,12 +1,18 @@
-%script to change inputs one by one and generate the base case at i == 13
+%%script to change inputs one by one and generate the base case at i == 13
 
-array = []; %empty set
+array = []; %empty cell array because otherwise there are issues with the string variable names
 
 for i=1:1:13
-
-temp = octavedriver(i,20);
-array = [array;-9999;temp];
-
-endfor
-
+[temp,temp2] = octavedriver(i,0.1);
+array(1:1:5,i) = temp(1:1:5,1);
+array(6,i) = temp2(9+20,1);
+array(7,i) = temp2(10+20,1);
+array(8,i) = temp2(11+20,1);
+array(9,i) = temp2(12+20,1);
+array(10:1:45,i) = temp(6:1:end,1); 
+%the 4 extra "variables" included here are inputs not measured. these are now included to calculate all the gains
+%the reason for the strange order of appending them is because of the layout of the connection csv file... 
+%this just keeps the variables consistent ito referencing
+end
+ 
 save -ascii inputgains.txt array
