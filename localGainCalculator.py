23c3e9bd5ca42bb44465d9172229c8781c2aec7b
St Elmo <stelmozors@gmail.com>
2012-03-29 01:10:37 +0200

    local gains are now calculated as a linear combination of gains affecting the variable. this took forever. it definitely works on a 4x4 system and probably works on the TE system although i still cant see the full output as python is too lazy to display it

--- localGainCalculator.py
+++ localGainCalculator.py
@@ -14,10 +14,11 @@ class localgains:
         self.createConnectionMatrix(nameofconn)
         self.createLocalChangeMatrix(nameofgains,states)
         self.createAverageLocalGainMatrix()
+        self.createLinearLocalGainMatrix(states)
     
-    def createAverageLocalGainMatrix(self):
+    def createAverageLocalGainMatrix(self): #this is not going to be used linear combination idea supercedes it
         #this method should calculate average gains based on the connectivity matrix and the local change matrix      
-        self.localgainmatrix = []
+        self.avelocalgainmatrix = [] #initialise the average local gain matrix
         for row in range(self.n):
             for col in range(self.n):
                 if self.connectionmatrix[row,col] == 1: #local gain = top/bottom where top is output change and bottom is input change
@@ -33,17 +34,58 @@ class localgains:
                         else: #if the input didn't change then the variable is being affect by something else
                             indextempgain = 0
                         tempgain = tempgain + abs(indextempgain) #is this absolute good? bad? what to do about gains cancelling each other?
-                    self.localgainmatrix.append(tempgain/12) #remember to average it out!
+                    self.avelocalgainmatrix.append(tempgain/endofrow) #remember to average it out! endofrow == number of runs
                 else:
-                    self.localgainmatrix.append(0)
+                    self.avelocalgainmatrix.append(0)
         from numpy import array
-        self.localgainmatrix = array(self.localgainmatrix).reshape(self.n,self.n)
+        self.avelocalgainmatrix = array(self.avelocalgainmatrix).reshape(self.n,self.n)
         
-        def normaliseGainMatrix(self): #the ranking algorithms expect a normalised input local gain matrix
-            pass
-                    
+        
+    def normaliseGainMatrix(self): #the ranking algorithms expect a normalised input local gain matrix
+        pass
     
-    def createLocalChangeMatrix(self, nameofgains,states):
+    def createLinearLocalGainMatrix(self, states): #note: not very efficient but it works... will try to improve later if necessary
+        from numpy import array, zeros, hstack
+        
+        self.linlocalgainmatrix = array(zeros((self.n, self.n)))  #initialise the linear local gain matrix
+        #this will be slightly inefficient at the moment... its this way to be sure of the method
+        self.localdiffmatrix = []        
+        for row in range(self.n):
+            for col in range(states-1):
+                temp = self.localchangematrix[row,col] - self.localchangematrix[row,states-1]
+                self.localdiffmatrix.append(temp)
+        
+        self.localdiffmatrix = array(self.localdiffmatrix).reshape(self.n,-1)
+        #now you have all the necessary inputs i.e. delta y and delta u (all in one matrix row wise)        
+        #now go in to the connection matrix and determine local gains row by row
+       
+        for row in range(self.n):
+           index = self.connectionmatrix[row,:].reshape(1,self.n) #see the next line as well... there seems to be a persistent wrapping error... this fixes it
+           if (max(max(index)) > 0): #crude but it works...    
+               compoundvec = self.localdiffmatrix[row,:].reshape(states-1, 1) #this basically transposes the array... for some reason i cant get it to work with tranpose()... probably a wrapping fault somewhere
+               #now you need to get uvec so that you may calculate the aprox gains
+               #note: rows == number of experiments       
+               for position in range(self.n):
+                   if index[0, position] == 1:
+                       temp = self.localdiffmatrix[position,:].reshape(-1,1) # dummy variable
+                       compoundvec = hstack((compoundvec,temp))
+                   else:
+                       pass #do nothing as the index will sort out the order of gain association
+               yvec = compoundvec[:,0].reshape(-1,1)
+               uvec = compoundvec[:,1:states]
+               import numpy as np #is the good?        
+               localgains =  np.linalg.lstsq(uvec,yvec)[0].reshape(1,-1)
+               tempindex = 0        
+               for position in range(self.n):
+                   if index[0, position] == 1:
+                       self.linlocalgainmatrix[row,position] = localgains[0,tempindex]
+                       tempindex = tempindex + 1
+                   else:
+                       pass #do nothing as the index will sort out the order of gain association
+           else:
+               pass
+    
+    def createLocalChangeMatrix(self, nameofgains,states): #NB NB NB the octave output inserts a white space before every row!!! if you use a different source file make sure it is as such or this method will either bomb out or neglect an entire column of data
         #this method should return an array of local changes   
         #the variable name are already in order due to foresight (hopefully)
         import csv
@@ -54,7 +96,7 @@ class localgains:
             for element in linefixed:
                 self.localchangematrix.append(float(element))
         from numpy import array
-        self.localchangematrix = array(self.localchangematrix).reshape(len(self.variables),states) #states = 12 input changes + 1 base case
+        self.localchangematrix = array(self.localchangematrix).reshape(len(self.variables),states) #states = 13 TE => 12 input changes + 1 base case
         #reasonably sure this works well
     
     def createConnectionMatrix(self, nameofconn): # assigns variable names and connection scheme
