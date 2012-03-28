439a43a1474521998a9b0830c8848cc22dc66a77
St Elmo <stelmozors@gmail.com>
2012-03-29 01:11:13 +0200

    the mini testing script updated

--- tester.py
+++ tester.py
@@ -7,10 +7,14 @@ Created on Mon Mar 26 23:24:42 2012
 
 from localGainCalculator import localgains
 
-test = localgains("connections.csv","inputgains.txt",13)
+#test = localgains("testConn.csv","testIG.txt",4) #dont forget to update the state-case variable
+test = localgains("connections.csv","inputgains45.txt",13)
 #print(test.connectionmatrix) #works
 #print(test.localchangematrix) #works
-print(test.localgainmatrix) #how do I see whats happening here?
+#print(test.avelocalgainmatrix) #works
+print(test.linlocalgainmatrix) #testing this
+
+
 
 
 
