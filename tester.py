dc7e151c47c494596416f843a7425e96c77e3971
St Elmo <stelmozors@gmail.com>
2012-03-29 20:58:48 +0200

    how on earth does the csv writing thing work?

--- tester.py
+++ tester.py
@@ -7,13 +7,19 @@ Created on Mon Mar 26 23:24:42 2012
 
 from localGainCalculator import localgains
 
-#test = localgains("testConn.csv","testIG.txt",4) #dont forget to update the state-case variable
-test = localgains("connections.csv","inputgains45.txt",13)
+test = localgains("testConn.csv","testIGprop.txt",4) #dont forget to update the state-case variable
+#test = localgains("connections.csv","inputgains45.txt",13)
 #print(test.connectionmatrix) #works
 #print(test.localchangematrix) #works
-#print(test.avelocalgainmatrix) #works
+#print(test.localdiffmatrix) #works
 print(test.linlocalgainmatrix) #testing this
+gainarray = test.linlocalgainmatrix
 
+import csv
+spam = csv.writer(open("openloopgainarray.txt",'w'))
+for row in gainarray:
+    print(row)
+print("done")
 
 
 
