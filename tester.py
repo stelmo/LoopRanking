eeab20d31fc4213778b752639ec16d9ebce53cb9
St Elmo <stelmozors@gmail.com>
2012-03-30 10:23:02 +0200

    tests new things

--- tester.py
+++ tester.py
@@ -6,20 +6,31 @@ Created on Mon Mar 26 23:24:42 2012
 """
 
 from localGainCalculator import localgains
+from RGABristol import RGA
 
-test = localgains("testFourConnections.csv","testFourIG.txt",5) #dont forget to update the state-case variable
-#test = localgains("connections.csv","inputgains45.txt",13)
-#print(test.connectionmatrix) #works
-#print(test.localchangematrix) #works
-#print(test.localdiffmatrix) #works
-print(test.linlocalgainmatrix) #testing this
-#gainarray = test.linlocalgainmatrix
+test = localgains("testOneConnections.csv","testOneIG.txt",4) #dont forget to update the state-case variable
+###test = localgains("connections.csv","inputgains45.txt",13)
+##print(test.connectionmatrix) #works
+##print(test.localchangematrix) #works
+###print(test.localdiffmatrix) #works
+#print(test.linlocalgainmatrix) #testing this
+#print(test.linlocalgainmatrixAV)
+print(test.avelocalgainmatrix)
+print(test.avelocalgainmatrixAV)
+##gainarray = test.linlocalgainmatrix
+#
+##import csv
+##spam = csv.writer(open("openloopgainarray.txt",'w'))
+##for row in gainarray:
+##    print(row)
+##print("done")
 
-#import csv
-#spam = csv.writer(open("openloopgainarray.txt",'w'))
-#for row in gainarray:
-#    print(row)
-#print("done")
+#localgains("testOneConnections.csv","testOneIG.txt",4)
+
+test = RGA("testOneConnections.csv","testOneIG.txt",4)
+print(test.bristol)
+
+
 
 
 
