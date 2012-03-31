e7e9afc8834ec8755dd5a9412a922d37c07f6eb5
St Elmo <stelmozors@gmail.com>
2012-03-31 22:51:17 +0200

    just using all the code with the TE

--- tester.py
+++ tester.py
@@ -6,38 +6,41 @@ Created on Mon Mar 26 23:24:42 2012
 """
 
 from localGainCalculator import localgains
+from gainRank import gRanking
 from RGABristol import RGA
+#the Tennessee Eastman Problem!
+step1 = localgains("connectionsTEfullyconnected.csv", "inputgains45Scaled.txt", 13) #dont forget to update the state-case variable
+import numpy as np
+localgainmatrix = step1.normaliseGainMatrix(step1.linlocalgainmatrix)
+variables = step1.variables
+np.savetxt("linearscaledlocalgainsTEfullyconnected.txt", localgainmatrix, delimiter = ",")
 
-#test = localgains("testOneConnections.csv","testOneIG.txt",4) #dont forget to update the state-case variable
-###test = localgains("connections.csv","inputgains45.txt",13)
-##print(test.connectionmatrix) #works
-##print(test.localchangematrix) #works
-###print(test.localdiffmatrix) #works
-#print(test.normaliseGainMatrix(test.linlocalgainmatrix)) #works
-##print(test.linlocalgainmatrix)
-#print(test.linlocalgainmatrix)
-#print(test.avelocalgainmatrixAV)
-##gainarray = test.linlocalgainmatrix
-#
-##import csv
-##spam = csv.writer(open("openloopgainarray.txt",'w'))
-##for row in gainarray:
-##    print(row)
-##print("done")
+#print(localgainmatrix)
+#print(variables)
+#this has all been saved to an excel
 
-#localgains("testOneConnections.csv","testOneIG.txt",4)
-#
-test = RGA("testOneConnections.csv","testOneIG.txt",4)
-print(test.openloopgainmatrix)
-print(test.variablecorrection)
-print(test.bristolmatrix)
+#lets see what the rankings look like!
+step2 = gRanking(localgainmatrix,variables)
+step2.showConnectRank()
+sortme = step2.rankDict
+for w in sorted(sortme, key=sortme.get):
+    print w, sortme[w]
 
 
+step3 = RGA("connectionsTEfullyconnected.csv", "inputgains45Scaled.txt", 13)
+#print(step3.pairedvariables)
+bristolarray = step3.bristolmatrix
+#print(step3.inputs)
+#print(step3.outputs)
+np.savetxt("bristolmatrixTEfullyconnected.txt", bristolarray, delimiter = ",")
 
-
-
-
-
+#test = RGA("testThreeConnections.csv","testThreeIG.txt", 5)
+#gainmatrix = test.openloopgainmatrix.transpose() #this is transposed for convenience
+#variables = test.variablecorrection
+#rgab = test.bristolmatrix
+#print(rgab)
+#print(variables)
+#print(test.pairedvariables)
 
 
 
