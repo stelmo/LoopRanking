fab79780d5a0e941b9f7885945790003b0e7feae
St Elmo <stelmozors@gmail.com>
2012-03-31 22:50:50 +0200

    scales 10% up and down of max and min respectively

--- scalingTEvariables.m
+++ scalingTEvariables.m
@@ -3,51 +3,15 @@
 %load file
 fromfile = load("inputgains45.txt");
 
-scales = [1;
-			2;
-			3;
-			4;
-			5;
-			6;
-			7;
-			8;
-			9;
-			10;
-			11;
-			12;
-			13;
-			14;
-			15;
-			16;
-			17;
-			18;
-			19;
-			20;
-			21;
-			21;
-			23;
-			24;
-			25;
-			26;
-			27;
-			28;
-			29;
-			30;
-			31;
-			32;
-			33;
-			34;
-			35;
-			36;
-			37;
-			38;
-			39;
-			40;
-			41;
-			42;
-			43;
-			44;
-			45];
+[r, c] = size(fromfile);
+
+for row = 1:1:r
+
+	maxinrow = max(fromfile(row, :))*1.1;
+	mininrow = min(fromfile(row, :))*0.9;		
+	scales(row,1) = maxinrow - mininrow;
+	
+end
 
 for i=1:1:13
 
