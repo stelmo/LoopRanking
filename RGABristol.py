1adfc08ee2030d091c3a763546bf2d89689fd84d
St Elmo <stelmozors@gmail.com>
2012-04-03 20:42:14 +0200

    fixed gain array. works correctly now

--- RGABristol.py
+++ RGABristol.py
@@ -7,100 +7,68 @@ Created on Fri Mar 30 00:45:09 2012
 
 #attempt at an implementation of the Bristol/ RGA method
 
-from localGainCalculator import localgains 
-
 class RGA:
     
-    def __init__(self,nameofconnections, nameoflocalchanges, states):
-        self.getOpenLoopGainArray(nameofconnections, nameoflocalchanges, states)
+    def __init__(self, variables, localdiffs, states, numberofinputs):
+        self.getOpenLoopGainArray(localdiffs, states, numberofinputs)
         self.calculateBristolmatrix()
-        self.calculateBristolpairings()
+        self.calculateBristolpairings(variables, numberofinputs)
     
-    def getOpenLoopGainArray(self, nameofconnections, nameoflocalchanges, states):
-        temp = localgains(nameofconnections, nameoflocalchanges, states)
-        self.allvariables = temp.variables #this is defined here and only used in the show method
-        #the reason being i dont feel like create duplicate objects
-        gainmatrix = temp.linlocalgainmatrix
+    def getOpenLoopGainArray(self, localdiffs, states, numberofinputs):
+        from numpy import where, array, empty        
+        [r, c] = localdiffs.shape
+        outputs = localdiffs[numberofinputs:r, :] 
+        [r, c] = outputs.shape
+        self.openloopmatrix = array(empty((numberofinputs, r)))
+        inputs = localdiffs[:numberofinputs, :] #array of inputs
+        [r, c] = inputs.shape
+        for row in inputs:
+            index = array(where(row != 0))[0,0] #this returns which input is being changed: column reference
+            outputsincolumn = outputs[:, index].transpose()
+            rowoutputs = outputsincolumn/row[index]
+            self.openloopmatrix[index, :] = rowoutputs
+        """ this method breaks up the data (experiment) array into two pieces: an input side and an output side
+        the input side is scanned and the indices where the change is not 0 (i.e. where the input was stepped) are stored
+        the output side is transposed; the change in input corrosponding to the output column vector is divided. 
+        these rows are then stacked together to generate an overall gain matrix"""
         
-        #delete full zero rowss
-        from numpy import vstack, hstack, transpose, array
-        flag = 0 # this makes sure that you can stack the empty initialised array
-        rowvariables = []        
-        for row in gainmatrix:
-            if sum(abs(row)) != 0:
-                if flag == 0:
-                    rowcorrection = hstack(row)
-                    flag = 1
-                else:
-                    rowcorrection = vstack((rowcorrection, row))
-                rowvariables.append(1)
-            else:
-                rowvariables.append(0)
-        
-        #delete full zero columns
-        flag = 0
-        colvariables = []
-        for col in transpose(rowcorrection):
-            if sum(abs(col)) != 0:
-                if flag == 0:
-                    colcorrection = hstack(col)
-                    flag = 1
-                else:
-                    colcorrection = vstack((colcorrection, col))
-                colvariables.append(1)
-            else:
-                colvariables.append(0)
-
-        self.openloopgainmatrix = colcorrection #you dont actually want to transpose this matrix
-        #because: RGA is outputs in columns and inputs in rows.
-        #they way we defined causality swaps this around. colcorrection is already transposed so
-        #it is in the correct format 
-        rowvariables = array(rowvariables).reshape(-1,1)
-        colvariables = array(colvariables).reshape(-1,1)
-        self.variablecorrection = hstack((rowvariables,colvariables))
-        #what i mean by variablecorrection is how you would read the rga
-        #some variables are cut because they influence nothing i.e. rows and columns of zero
-        #this works
     
     def calculateBristolmatrix(self):
         #so basically the inverse is either square, left or right
-        [r,c] = self.openloopgainmatrix.shape #because not necessarily square
+        [r,c] = self.openloopmatrix.shape #because not necessarily square
         self.bristolmatrix = []
         import numpy as np
         from numpy import array
         if (r==c): #square
-            if (np.linalg.det(self.openloopgainmatrix) != 0):
-                R = np.linalg.inv(self.openloopgainmatrix).transpose()
-                for gij, rij in zip(self.openloopgainmatrix.flat, R.flat):
+            if (np.linalg.det(self.openloopmatrix) != 0):
+                R = np.linalg.inv(self.openloopmatrix).transpose()
+                for gij, rij in zip(self.openloopmatrix.flat, R.flat):
                     self.bristolmatrix.append(gij*rij)
                 self.bristolmatrix = array(self.bristolmatrix).reshape(r,r)
             else:
                 print("Sorry, the open loop gain matrix is singular")
         else: #seems pinv calculates the moore-penrose inverse... so left or right inverse is not a problem
-            R = np.linalg.pinv(self.openloopgainmatrix).transpose()
-            for gij, rij in zip(self.openloopgainmatrix.flat, R.flat):
+            R = np.linalg.pinv(self.openloopmatrix).transpose()
+            for gij, rij in zip(self.openloopmatrix.flat, R.flat):
                 self.bristolmatrix.append(gij*rij)
             self.bristolmatrix = array(self.bristolmatrix).reshape(r,c)
         #this all works and generates believable results
-            
+        """ this method calculates the RG array. """   
         
-    def calculateBristolpairings(self):
-        n = len(self.allvariables)
-        self.outputs = []
-        self.inputs = []
+    def calculateBristolpairings(self,vararrs,numofinputs):
+        inputvars = vararrs[:numofinputs]        
+        outputvars = vararrs[numofinputs:]
+        [r, c] = self.bristolmatrix.shape
         self.pairedvariables = []
-        for index in range(n):
-            if self.variablecorrection[index, 0] == 1:
-                self.outputs.append(self.allvariables[index]) 
-            if self.variablecorrection[index, 1] == 1:
-                self.inputs.append(self.allvariables[index])
-        [r,c] = self.bristolmatrix.shape
         for row in range(r):
             for col in range(c):
-                if self.bristolmatrix[row,col] >= 0.5:#this 0.5 comes from theory
-                    self.pairedvariables.append(self.outputs[col])
-                    self.pairedvariables.append(self.inputs[row])
+                if self.bristolmatrix[row,col] > 0.5: #from theory
+                    self.pairedvariables.append(inputvars[row])
+                    self.pairedvariables.append(outputvars[col])
         from numpy import array
         self.pairedvariables = array(self.pairedvariables).reshape(-1,2)
+        """ this just generates a matrix of variables which should be paired using the RGA method"""
+        
+        
             
         
