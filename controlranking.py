ec1d2f9619d664f57895b88ec2e9351d026b8c0c
St Elmo <stelmozors@gmail.com>
2012-05-05 20:44:43 +0200

    part 2 of the project's master class (will be similiar in nature to visualise.py

---
+++ controlranking.py
@@ -0,0 +1,19 @@
+'''
+Created on 05 May 2012
+
+@author: St Elmo Wilken
+'''
+
+class loopranking:
+    """This class will:
+    1) Rank the importance of nodes in a system with control
+    1.a) Use the local gains to determine importance
+    1.b) Use partial correlation information to determine importance
+    2) Determine the change of importance when variables change
+    """
+    
+    def __init__(self):
+        """This constructor implement 1-2 as stated above"""
+        pass
+    
+    
